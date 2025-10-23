bdd665e945e8a859ed62419696cf462fe2533d29
403-ko2 <177669720+403-ko2@users.noreply.github.com>
2025-10-23 15:31:58 -0400

    added a navbar file

---
+++ Winked/app/(tabs)/home.tsx
@@ -0,0 +1,8 @@
+import React, { useState } from 'react';
+import { View, Text, FlatList, TouchableOpacity, StyleSheet } from 'react-native';
+import { Link } from 'expo-router';
+
+export default function Home(){
+
+
+}
---
+++ Winked/app/(tabs)/rewrite.tsx
@@ -0,0 +1,3 @@
+import React, { useState } from 'react';
+import { View, Text, FlatList, TouchableOpacity, StyleSheet } from 'react-native';
+import { Link } from 'expo-router';
